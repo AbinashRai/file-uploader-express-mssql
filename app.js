badb627d949cecd41a8af2e0e671885e3eaff494
Abinash Rai <abinashbantawa1905@gmail.com>
2023-08-15 13:43:44 +0545

    built the server and sucessfully tested the server in postman

--- app.js
+++ app.js
@@ -8,4 +8,16 @@ const uploader = multer({
   storage: multer.memoryStorage(),
 });
 
-app.post("/upload", uploader.single);
+app.post("/upload", uploader.single("file"), async (req, res) => {
+  const file = req.file;
+
+  fs.writeFileSync(__dirname + "/" + file.originalname, file.buffer);
+
+  res.status(200).json({
+    status: "ok",
+  });
+});
+
+app.listen(8000, () => {
+  console.log("Server has been started");
+});
