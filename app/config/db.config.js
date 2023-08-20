4fb5aaeada8372cb6b418f23b2a9d4bf9c86a9ab
Abinash Rai <abinashbantawa1905@gmail.com>
2023-08-20 16:21:29 +0545

    resolving errors

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,23 +1,14 @@
-const { Sequelize } = require("sequelize");
-const dbConfig = require("./db.config");
-
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: "localhost",
+module.exports = {
+  HOST: "localhost",
+  PORT: 1433,
+  USER: "sa",
+  PASSWORD: "admin123",
+  DB: "testdb",
   dialect: "mssql",
-});
-
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log("Connection has been established successfully.");
-  })
-  .catch((error) => {
-    console.error("Unable to connect to the database:", error);
-  });
-
-sequelize
-  .sync({ force: true })
-  .then(() => console.log("modules are syncronized"))
-  .catch((error) => console.log(error));
-
-module.exports = sequelize;
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
