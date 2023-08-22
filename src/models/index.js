121ca853c1e2474a504d5bf83e618a177cfae860
Abinash Rai <abinashbantawa1905@gmail.com>
2023-08-22 16:52:29 +0545

    resolving issues with the database

--- src/models/index.js
+++ src/models/index.js
@@ -1,22 +1,22 @@
-// const dbConfig = require("../config/db.config.js");
+const dbConfig = require("../config/db.config.js");
 
-// const Sequelize = require("sequelize");
-// const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-//   host: dbConfig.HOST,
-//   port: dbConfig.PORT,
-//   dialect: dbConfig.dialect,
-//   pool: {
-//     max: dbConfig.pool.max,
-//     min: dbConfig.pool.min,
-//     acquire: dbConfig.pool.acquire,
-//     idle: dbConfig.pool.idle,
-//   },
-// });
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  port: dbConfig.PORT,
+  dialect: dbConfig.dialect,
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle,
+  },
+});
 
-// const db = {};
+const db = {};
 
-// db.Sequelize = Sequelize;
-// db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
 
-// db.images = require("./image.model.js")(sequelize, Sequelize);
-// module.exports = db;
+db.images = require("./image.model.js")(sequelize, Sequelize);
+module.exports = db;
